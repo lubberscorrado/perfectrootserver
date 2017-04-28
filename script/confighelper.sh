9ec9f3f256198a4cd76a33b5b7620d40c47e39a9
BoBBer446 <eXestend@gmx.de>
2017-04-28 11:23:26 +0200

    Update confighelper.sh

--- script/confighelper.sh
+++ script/confighelper.sh
@@ -25,12 +25,14 @@
 ##  DO NOT MODIFY, JUST DON'T! ##
 #################################
 
-
 confighelper_userconfig() {
-cho "${info} Start confighelper for Userconfig" | awk '{ print strftime("[%H:%M:%S] |"), $0 }'
+#echo "${info} Start confighelper...." | awk '{ print strftime("[%H:%M:%S] |"), $0 }'
 
 # Small function...
 CHECK_E_MAIL="^[a-z0-9!#\$%&'*+/=?^_\`{|}~-]+(\.[a-z0-9!#$%&'*+/=?^_\`{|}~-]+)*@([a-z0-9]([a-z0-9-]*[a-z0-9])?\.)+[a-z0-9]([a-z0-9-]*[a-z0-9])?\$"
+
+
+	echo "Start Confighelper for Userconfig"
 	#Userconfig
 	CONFIG_COMPLETED="0"
 	TIMEZONE="Europe/Berlin"
@@ -62,6 +64,9 @@ CHECK_E_MAIL="^[a-z0-9!#\$%&'*+/=?^_\`{|}~-]+(\.[a-z0-9!#$%&'*+/=?^_\`{|}~-]+)*@
 	MYSQL_HOSTNAME="localhost"
 	IAMEXPERT="0"
 
+
+
+
 	read -p "Are you an expert? Please type y or n:" IAMEXPERT
 		if [ "$IAMEXPERT" = "y" ]; then
 			IAMEXPERT="1"
@@ -74,27 +79,29 @@ CHECK_E_MAIL="^[a-z0-9!#\$%&'*+/=?^_\`{|}~-]+(\.[a-z0-9!#$%&'*+/=?^_\`{|}~-]+)*@
 		fi
 
 
-	# --------------------------------
+	# ----------------------------------------------------------------
 	timezones="
 	Europe/Berlin
 	America/New_York
 	"
 	select choosetimezone in $timezones; do
 		if [ "$choosetimezone" = "Europe/Berlin" ]; then
+		TIMEZONE="$choosetimezone"
 			break;
 		fi
 
 		if [ "$choosetimezone" = "America/New_York" ]; then
+		TIMEZONE="$choosetimezone"
 			break;
 		fi
 	done
 	echo "[Finishd] Your Timezone is $choosetimezone"
-	# --------------------------------
+	# ----------------------------------------------------------------
 
 	read -p "Enter your Domain without http://:" MYDOMAIN
 	echo "[Finishd] Your Domain is $MYDOMAIN"
 
-	# --------------------------------
+	# ----------------------------------------------------------------
 	read -p "Do you want to Use SSL? Please type y or n:" USE_VALID_SSL
 		if [ "$USE_VALID_SSL" = "y" ]; then
 			USE_VALID_SSL="1"
@@ -117,7 +124,7 @@ CHECK_E_MAIL="^[a-z0-9!#\$%&'*+/=?^_\`{|}~-]+(\.[a-z0-9!#$%&'*+/=?^_\`{|}~-]+)*@
 			USE_VALID_SSL="0"
 			echo "[Finishd] You dont use SSL"
 		fi
-	# --------------------------------
+	# ----------------------------------------------------------------
 	read -p "Do you want to Use Mailserver? Please type y or n:" USE_MAILSERVER
 		if [ "$USE_MAILSERVER" = "y" ]; then
 			USE_MAILSERVER="1"
@@ -141,7 +148,7 @@ CHECK_E_MAIL="^[a-z0-9!#\$%&'*+/=?^_\`{|}~-]+(\.[a-z0-9!#$%&'*+/=?^_\`{|}~-]+)*@
 			echo "[Finishd] You dont use Mailserver"
 		fi
 
-	# --------------------------------
+	# ----------------------------------------------------------------
 	read -p "Do you want to Use PHP5 oder PHP7? Please type 5 or 7 " USE_PHPVERSION
 		if [ "$USE_PHPVERSION" = "5" ]; then
 			USE_PHP5="1"
@@ -154,7 +161,7 @@ CHECK_E_MAIL="^[a-z0-9!#\$%&'*+/=?^_\`{|}~-]+(\.[a-z0-9!#$%&'*+/=?^_\`{|}~-]+)*@
 			USE_PHP5="0"
 			echo "[Finishd] You use PHP-Version 7"
 		fi
-	# --------------------------------
+	# ----------------------------------------------------------------
 	read -p "Do you want to Use PHPMyAdmin? Please type y or n:" USE_PMA
 		if [ "$USE_PMA" = "y" ]; then
 			USE_PMA="1"
@@ -177,7 +184,7 @@ CHECK_E_MAIL="^[a-z0-9!#\$%&'*+/=?^_\`{|}~-]+(\.[a-z0-9!#$%&'*+/=?^_\`{|}~-]+)*@
 			USE_PMA="0"
 			echo "[Finishd] You dont use PHPMyAdmin"
 		fi
-	# --------------------------------
+	# ----------------------------------------------------------------
 	read -p "Do you want allow http-connections? Please type y or n:" ALLOWHTTPCONNECTIONS
 		if [ "$ALLOWHTTPCONNECTIONS" = "y" ]; then
 			ALLOWHTTPCONNECTIONS="1"
@@ -189,17 +196,17 @@ CHECK_E_MAIL="^[a-z0-9!#\$%&'*+/=?^_\`{|}~-]+(\.[a-z0-9!#$%&'*+/=?^_\`{|}~-]+)*@
 			echo "[Finishd] You dont allow http-connections"
 		fi
 
-	# --------------------------------
+	# ----------------------------------------------------------------
 	read -p "Please choose an user for HTTP-Auth login:" PMA_HTTPAUTH_USER
-	# --------------------------------
+	# ----------------------------------------------------------------
 	read -p "Please choose an user for MYSQL PHPMyAdmin Login:" MYSQL_PMADB_USER
-	# --------------------------------
+	# ----------------------------------------------------------------
 	read -p "Please choose an user for MYSQL PHPMyAdmin Name:" MYSQL_PMADB_NAME
 
 
-	# --------------------------------
+	# ----------------------------------------------------------------
 	if [ "$IAMEXPERT" = "1" ]; then
-		# --------------------------------
+	# ----------------------------------------------------------------
 		read -p "Do you want use Cloudflare? Please type y or n:" CLOUDFLARE
 			if [ "$CLOUDFLARE" = "y" ]; then
 				CLOUDFLARE="1"
@@ -210,18 +217,23 @@ CHECK_E_MAIL="^[a-z0-9!#\$%&'*+/=?^_\`{|}~-]+(\.[a-z0-9!#$%&'*+/=?^_\`{|}~-]+)*@
 				CLOUDFLARE="0"
 				echo "[Finishd] You dont use Cloudflare"
 			fi
-		# --------------------------------
-		read -p "Do you want use HIGH SECURITY? Please type y or n:" HIGH_SECURITY
-			if [ "$HIGH_SECURITY" = "y" ]; then
-				HIGH_SECURITY="1"
-				echo "[Finishd] You use HIGH SECURITY"
-			fi
+	# ----------------------------------------------------------------
+		if [ "$ALLOWHTTPCONNECTIONS" = "0" ]; then
+			read -p "Do you want use HIGH SECURITY? Please type y or n:" HIGH_SECURITY
+				if [ "$HIGH_SECURITY" = "y" ]; then
+					HIGH_SECURITY="1"
+					echo "[Finishd] You use HIGH SECURITY"
+				fi
 
-			if [ "$HIGH_SECURITY" = "n" ]; then
-				HIGH_SECURITY="0"
-				echo "[Finishd] You dont use HIGH SECURITY"
-			fi
-		# --------------------------------
+				if [ "$HIGH_SECURITY" = "n" ]; then
+					HIGH_SECURITY="0"
+					echo "[Finishd] You dont use HIGH SECURITY"
+				fi
+
+		else
+		HIGH_SECURITY="0"
+		fi
+	# ----------------------------------------------------------------
 		read -p "Do you want use Debug Mode? Please type y or n:" DEBUG_IS_SET
 			if [ "$DEBUG_IS_SET" = "y" ]; then
 				DEBUG_IS_SET="1"
@@ -233,7 +245,7 @@ CHECK_E_MAIL="^[a-z0-9!#\$%&'*+/=?^_\`{|}~-]+(\.[a-z0-9!#$%&'*+/=?^_\`{|}~-]+)*@
 				echo "[Finishd] You dont use Debug Mode"
 			fi
 	fi
-	# --------------------------------
+	# ----------------------------------------------------------------
 	NEED_ADDONCONFIG="1"
 	read -p "Do You need Addonconfig? Please type y or n:" NEED_ADDONCONFIG
 		if [ "$NEED_ADDONCONFIG" = "y" ]; then
@@ -243,6 +255,7 @@ CHECK_E_MAIL="^[a-z0-9!#\$%&'*+/=?^_\`{|}~-]+(\.[a-z0-9!#$%&'*+/=?^_\`{|}~-]+)*@
 
 		if [ "$NEED_ADDONCONFIG" = "n" ]; then
 			NEED_ADDONCONFIG="0"
+			ADDONCONFIG_COMPLETED="0"
 			echo "[Finishd] All is done!"
 		fi
 
@@ -292,9 +305,382 @@ END
 
 confighelper_addonconfig() {
 echo "Start Confighelper for Addonconfig"
-echo "soon as possible"
+
+ADDONCONFIG_COMPLETED="1"
+ADD_NEW_SITE="0"
+MYOTHERDOMAIN="MYOTHERDOMAIN.tld"
+DISABLE_ROOT_LOGIN="0"
+SSHUSER="YOURSSHUSER"
+USE_TEAMSPEAK="0"
+USE_MINECRAFT="0"
+USE_AJENTI="0"
+AJENTI_PASS="generatepw"
+USE_PIWIK="0"
+USE_VSFTPD="0"
+FTP_USERNAME="ftpsuperuser"
+USE_OPENVPN="0"
+KEY_COUNTRY="0" 	#(Example: DE for Germany)
+KEY_PROVINCE="0"	#(Example: Hessen)
+KEY_CITY="0"		#(Example: Frankfurt)
+KEY_EMAIL="0"		#(Example: your@email)
+SERVER_IP="0"		#(Example 92.12.34.567)
+USE_PRESTASHOP="0"
+PRESTASHOP_VERSION="1.6.1.12"
+PRESTASHOPDOMAIN="domain.tld"
+PRESTASHOP_OWNER_FIRSTNAME="Perfect"
+PRESTASHOP_OWNER_LASTNAME="Rootserver"
+PRESTASHOP_OWNER_EMAIL="prestashop@perfectrootserver.de"
+PRESTASHOPS_NAME="perfectrootserver script"
+PRESTASHOP_INSTALL_FOLDER_NAME="CHANGEME"
+PRESTASHOP_LANGUAGE="de"
+PRESTASHOP_COUNTRY="de"
+PRESTASHOP_DB_SERVER="localhost"
+PRESTASHOP_TIMEZONE="berlin"
+PRESTASHOP_DB_CLEAR="1"
+PRESTASHOP_CREATE_DB="1"
+PRESTASHOP_SHOW_LICENSE="0"
+PRESTASHOP_NEWSLETTER="1"
+PRESTASHOP_SEND_EMAIL="1"
+ADDONCONFIG_COMPLETED="0"
 
 
+	# ----------------------------------------------------------------
+	read -p "Do you want to Add a new Site? Please type y or n:" ADD_NEW_SITE
+	if [ "$ADD_NEW_SITE" = "y" ]; then
+			ADD_NEW_SITE="1"
+			echo "[Finishd] You want to add a new Site."
+				# --------------------------------
+				read -p "Please Type your Domain without http://www.:" MYOTHERDOMAIN
+				echo "[Finishd] Your second Domain is: $MYOTHERDOMAIN"
+	fi
+
+	if [ "$ADD_NEW_SITE" = "n" ]; then
+			MYOTHERDOMAIN="0"
+			echo "[Finishd] You dont add a new site"
+	fi
+
+	# ----------------------------------------------------------------
+	read -p "Do you want to disable root Login? Please type y or n:" DISABLE_ROOT_LOGIN
+	if [ "$DISABLE_ROOT_LOGIN" = "y" ]; then
+			DISABLE_ROOT_LOGIN="1"
+			echo "[Finishd] You want to disable root login."
+				# --------------------------------
+				read -p "Please Type your new SSH User:" SSHUSER
+				echo "[Finishd] Your new SSH USer is: $SSHUSER"
+	fi
+
+	if [ "$DISABLE_ROOT_LOGIN" = "n" ]; then
+			DISABLE_ROOT_LOGIN="0"
+			echo "[Finishd] You dont disable root Login. Your User to login is: root"
+	fi
+
+	# ----------------------------------------------------------------
+	read -p "Do you want use TeamSpeak? Please type y or n:" USE_TEAMSPEAK
+	if [ "$USE_TEAMSPEAK" = "y" ]; then
+			USE_TEAMSPEAK="1"
+			echo "[Finishd] You install Teamspeack."
+	fi
+
+	if [ "$USE_TEAMSPEAK" = "n" ]; then
+			USE_TEAMSPEAK="0"
+			echo "[Finishd] You dont use Teamspeak"
+	fi
+
+	# ----------------------------------------------------------------
+	read -p "Do you want use Minecraft? Please type y or n:" USE_MINECRAFT
+	if [ "$USE_MINECRAFT" = "y" ]; then
+			USE_MINECRAFT="1"
+			echo "[Finishd] You install Minecraft."
+	fi
+
+	if [ "$USE_MINECRAFT" = "n" ]; then
+			USE_MINECRAFT="0"
+			echo "[Finishd] You dont use Minecraft"
+	fi
+
+	# ----------------------------------------------------------------
+
+	if [ "$USE_VALID_SSL" = "1" ]; then
+		read -p "Do you want use Ajenti? Please type y or n:" USE_AJENTI
+			if [ "$USE_AJENTI" = "y" ]; then
+			USE_AJENTI="1"
+			echo "[Finishd] You install Ajenti."
+		fi
+
+		if [ "$USE_AJENTI" = "n" ]; then
+				USE_AJENTI="0"
+				echo "[Finishd] You dont use Ajenti"
+		fi
+
+	fi
+
+	# ----------------------------------------------------------------
+	read -p "Do you want use Piwik? Please type y or n:" USE_PIWIK
+	if [ "$USE_PIWIK" = "y" ]; then
+			USE_PIWIK="1"
+			echo "[Finishd] You install Piwik."
+	fi
+
+	if [ "$USE_PIWIK" = "n" ]; then
+			USE_PIWIK="0"
+			echo "[Finishd] You dont use Piwik"
+	fi
+
+	# ----------------------------------------------------------------
+	read -p "Do you want use VSFTPd? Please type y or n:" USE_VSFTPD
+	if [ "$USE_VSFTPD" = "y" ]; then
+			USE_VSFTPD="1"
+			echo "[Finishd] You install VSFTPd."
+				# --------------------------------
+					while true
+					do
+						read -p "Please type your ftp User. Use only a-z!:" FTP_USERNAME
+						if [[ $FTP_USERNAME =~ ^[a-z]+$ ]]; then
+							echo "[Finishd] Your FTP USername is $FTP_USERNAME."
+							break
+						else
+							echo "[Big Error] Should we perhaps learn something about lowercase letters?"
+						fi
+					done
+	fi
+
+	if [ "$USE_VSFTPD" = "n" ]; then
+			USE_VSFTPD="0"
+			echo "[Finishd] You dont use VSFTPd"
+	fi
+
+	# ----------------------------------------------------------------
+	read -p "Do you want use OPENVPN? Please type y or n:" USE_OPENVPN
+	if [ "$USE_OPENVPN" = "y" ]; then
+		USE_OPENVPN="1"
+		echo "[Finishd] You use OPENVPN"
+			# --------------------------------
+			SERVER_IP=$(ip route get 8.8.8.8 | head -1 | cut -d' ' -f8)
+
+			while true
+			do
+				read -p "Enter your valid E-Mail address: " KEY_EMAIL
+				if [[ "$KEY_EMAIL" =~ $CHECK_E_MAIL ]];then
+					echo "[Finishd] Your E-Mail address is $KEY_EMAIL"
+					break
+				else
+					echo "[ERROR] Should we again practice how an e-mail address looks?"
+				fi
+			done
+
+			read -p "Please type your COUNTRY. Example: DE for Germany:" KEY_COUNTRY
+			read -p "Please type your PROVINCE. Example: Hessen:" KEY_PROVINCE
+			read -p "Please type your CITY. Example: Frankfurt:" KEY_CITY
+
+	fi
+
+	if [ "$USE_OPENVPN" = "n" ]; then
+		USE_OPENVPN="0"
+		echo "[Finishd] You dont use OPENVPN"
+	fi
+
+
+	# ----------------------------------------------------------------
+	# NOT READY!
+	# DO NOT USE PRESTASHOP!
+	# ---------------------------------------------------------------#
+		#read -p "Do you want use Prestashop E-Commerce System? Please type y or n:" USE_PRESTASHOP
+		USE_PRESTASHOP="n"
+	if [ "$USE_PRESTASHOP" = "y" ]; then
+		USE_PRESTASHOP="1"
+		echo "[Finishd] You use Prestashop E-Commerce System"
+			# --------------------------------
+			PRESTASHOP_VERSION="1.6.1.12"
+			PRESTASHOPDOMAIN="domain.tld"
+			PRESTASHOP_OWNER_FIRSTNAME="Perfect"
+			PRESTASHOP_OWNER_LASTNAME="Rootserver"
+			PRESTASHOP_OWNER_EMAIL="prestashop@perfectrootserver.de"
+			PRESTASHOPS_NAME="perfectrootserver script"
+			PRESTASHOP_INSTALL_FOLDER_NAME="CHANGEME"
+			PRESTASHOP_LANGUAGE="de"
+			PRESTASHOP_COUNTRY="de"
+
+			PRESTASHOP_DB_SERVER="localhost"
+
+
+			PRESTASHOP_TIMEZONE="berlin"
+			PRESTASHOP_DB_CLEAR="1"
+			PRESTASHOP_CRYPT_PRF="${PRESTASHOP_CRYPT_PRF}"
+			PRESTASHOP_DB_NAME="${PRESTASHOP_DB_NAME}"
+			PRESTASHOP_CREATE_DB="1"
+			PRESTASHOP_SHOW_LICENSE="0"
+			PRESTASHOP_NEWSLETTER="1"
+			PRESTASHOP_SEND_EMAIL="1"
+
+
+
+			# --------------------------------
+			read -p "Do you want clear DB? Please type y or n:" PRESTASHOP_DB_CLEAR
+			if [ "$PRESTASHOP_DB_CLEAR" = "y" ]; then
+				PRESTASHOP_DB_CLEAR="1"
+				echo "[Finishd] Deleting the database"
+			fi
+
+			if [ "$PRESTASHOP_DB_CLEAR" = "n" ]; then
+				PRESTASHOP_DB_CLEAR="0"
+				echo "[Finishd] Deleting no database"
+			fi
+
+			# --------------------------------
+			# Todo
+			# Check var after installation!
+			select choosetimezone in $timezones; do
+				if [ "$choosetimezone" = "Europe/Berlin" ]; then
+				PRESTASHOP_TIMEZONE="$choosetimezone"
+					break;
+				fi
+
+				if [ "$choosetimezone" = "America/New_York" ]; then
+				PRESTASHOP_TIMEZONE="$choosetimezone"
+					break;
+				fi
+			done
+			echo "[Finishd] Your Timezone is $choosetimezone"
+
+
+			# --------------------------------
+			# Todo
+			# Add function to check prf
+			PRESTASHOP_DB_ENGINE="
+			InnoDB
+			MyISAM
+			"
+			read -p "Please choose an Präfix:" PRESTASHOP_CRYPT_PRF
+			echo "[Finishd] Your new DB Präfix is $PRESTASHOP_CRYPT_PRF"
+
+			# --------------------------------
+			echo "Please choose an DB engine"
+			select chooseengine in $PRESTASHOP_DB_ENGINE; do
+				if [ "$chooseengine" = "Europe/Berlin" ]; then
+					break;
+				fi
+
+				if [ "$chooseengine" = "America/New_York" ]; then
+					break;
+				fi
+			done
+			echo "[Finishd] Your Timezone is $chooseengine"
+
+			# --------------------------------
+			read -p "Do you want create an Database (It is recommended)? Please type y or n:" PRESTASHOP_CREATE_DB
+			if [ "$PRESTASHOP_CREATE_DB" = "y" ]; then
+				PRESTASHOP_CREATE_DB="1"
+				echo "[Finishd] You create an new Database."
+					# --------------------------------
+					read -p "Please type your DB name. Please type y or n:" PRESTASHOP_DB_NAME
+			fi
+
+			if [ "$PRESTASHOP_CREATE_DB" = "n" ]; then
+				PRESTASHOP_CREATE_DB="0"
+				echo "[Finishd] You dont create an new Database."
+			fi
+
+			# --------------------------------
+			read -p "Do you want to show the license from Prestashops E-Commerce System? Please type y or n:" PRESTASHOP_SHOW_LICENSE
+			if [ "$PRESTASHOP_SHOW_LICENSE" = "y" ]; then
+				PRESTASHOP_SHOW_LICENSE="1"
+				echo "[Finishd] You see the license"
+			fi
+
+			if [ "$PRESTASHOP_SHOW_LICENSE" = "n" ]; then
+				PRESTASHOP_SHOW_LICENSE="0"
+				echo "[Finishd] You dont see the license"
+			fi
+
+			# --------------------------------
+			read -p "Du you want subscribe your own Newsletter? Please type y or n:" PRESTASHOP_NEWSLETTER
+			if [ "$PRESTASHOP_NEWSLETTER" = "y" ]; then
+				PRESTASHOP_NEWSLETTER="1"
+				echo "[Finishd] You subscribe your Newsletter"
+			fi
+
+			if [ "$PRESTASHOP_NEWSLETTER" = "n" ]; then
+				PRESTASHOP_NEWSLETTER="0"
+				echo "[Finishd] You dont subscribe your Newsletter"
+			fi
+
+			# --------------------------------
+			read -p "Do you wish an E-Mail after Setup? Please type y or n:" PRESTASHOP_SEND_EMAIL
+			if [ "$PRESTASHOP_SEND_EMAIL" = "y" ]; then
+				PRESTASHOP_SEND_EMAIL="1"
+				echo "[Finishd] You become an E-Mail after installation"
+			fi
+
+			if [ "$PRESTASHOP_SEND_EMAIL" = "n" ]; then
+				PRESTASHOP_SEND_EMAIL="0"
+				echo "[Finishd] You become no E-Mail after installation"
+			fi
+			# --------------------------------
+
+	fi
+
+	if [ "$USE_PRESTASHOP" = "n" ]; then
+		USE_PRESTASHOP="0"
+		echo "[Finishd] You dont use Prestashop E-Commerce System"
+	fi
+
+
+
+# Write Vars to Config file:
+
+rm -rf ~/configs/addonconfig.cfg
+cat >> ~/configs/addonconfig.cfg <<END
+#-----------------------------------------------------------#
+############### Config File from Confighelper ###############
+#-----------------------------------------------------------#
+
+ADDONCONFIG_COMPLETED="${CONFIG_COMPLETED}"
+ADD_NEW_SITE="${ADD_NEW_SITE}"
+MYOTHERDOMAIN="${MYOTHERDOMAIN}"
+DISABLE_ROOT_LOGIN="${DISABLE_ROOT_LOGIN}"
+SSHUSER="${SSHUSER}"
+USE_TEAMSPEAK="${USE_TEAMSPEAK}"
+USE_MINECRAFT="${USE_MINECRAFT}"
+USE_AJENTI="${USE_AJENTI}"
+AJENTI_PASS="generatepw"
+USE_PIWIK="${USE_PIWIK}"
+USE_VSFTPD="${USE_VSFTPD}"
+FTP_USERNAME="${FTP_USERNAME}"
+USE_OPENVPN="${USE_OPENVPN}"
+KEY_COUNTRY="${KEY_COUNTRY}"
+KEY_PROVINCE="${KEY_PROVINCE}"
+KEY_CITY="${KEY_CITY}"
+KEY_EMAIL="${KEY_EMAIL}"
+SERVER_IP="${SERVER_IP}"
+
+USE_PRESTASHOP="${USE_PRESTASHOP}"
+PRESTASHOP_VERSION="${PRESTASHOP_VERSION}"
+PRESTASHOPDOMAIN="${PRESTASHOPDOMAIN}"
+PRESTASHOP_OWNER_FIRSTNAME="${PRESTASHOP_OWNER_FIRSTNAME}"
+PRESTASHOP_OWNER_LASTNAME="${PRESTASHOP_OWNER_LASTNAME}"
+PRESTASHOP_OWNER_EMAIL="${PRESTASHOP_OWNER_EMAIL}"
+PRESTASHOPS_NAME="${PRESTASHOPS_NAME}"
+PRESTASHOP_INSTALL_FOLDER_NAME="${PRESTASHOP_INSTALL_FOLDER_NAME}"
+PRESTASHOP_LANGUAGE="${PRESTASHOP_LANGUAGE}"
+PRESTASHOP_COUNTRY="${PRESTASHOP_COUNTRY}"
+PRESTASHOP_DB_SERVER="localhost"
+PRESTASHOP_TIMEZONE="${PRESTASHOP_TIMEZONE}"
+PRESTASHOP_DB_CLEAR="${PRESTASHOP_DB_CLEAR}"
+PRESTASHOP_CREATE_DB="${PRESTASHOP_CREATE_DB}"
+PRESTASHOP_SHOW_LICENSE="${PRESTASHOP_SHOW_LICENSE}"
+PRESTASHOP_NEWSLETTER="${PRESTASHOP_NEWSLETTER}"
+PRESTASHOP_SEND_EMAIL="${PRESTASHOP_SEND_EMAIL}"
+PRESTASHOP_DB_NAME="${PRESTASHOP_DB_NAME}"
+PRESTASHOP_CRYPT_PRF="${PRESTASHOP_CRYPT_PRF}"
+PRESTASHOP_DB_ENGINE="${chooseengine}"
+
+
+
+
+#-----------------------------------------------------------#
+############### Config File from Confighelper ###############
+#-----------------------------------------------------------#
+END
 }
 
 
@@ -304,3 +690,10 @@ confighelper_userconfig
 confighelper_addonconfig
 
 }
+
+#Run on install.sh
+#confighelper
+
+
+#source ~/configs/userconfig.cfg
+#source ~/configs/addonconfig.cfg
