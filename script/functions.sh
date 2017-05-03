ea2bf03b83db3a5b55dfcf4248a295a416b2987b
BoBBer446 <eXestend@gmx.de>
2017-05-03 17:22:54 +0200

    fixed "finished"

--- script/functions.sh
+++ script/functions.sh
@@ -43,7 +43,7 @@ warn="$(yellowb [WARN] -)"
 error="$(redb [ERROR] -)"
 fyi="$(pinkb [INFO] -)"
 ok="$(greenb [OKAY] -)"
-Finishd="$(greenb [Finishd] -)"
+finished="$(greenb [Finished] -)"
 
 IPADR=$(ip route get 8.8.8.8 | head -1 | cut -d' ' -f8)
 INTERFACE=$(ip route get 8.8.8.8 | head -1 | cut -d' ' -f5)
